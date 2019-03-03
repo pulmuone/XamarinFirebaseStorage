8e4d9d065ccb108ed06a71490af1732b287ad14a
gwise <pulmuone.ji@gmail.com>
2019-03-04 01:19:24 +0900

    Add project files.

---
+++ XamarinFirebaseStorage/XamarinFirebaseStorage.Android/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.App;
+using Android;
+using Android.Support.Design.Widget;
+
+namespace XamarinFirebaseStorage.Droid
+{
+    [Activity(Label = "XamarinFirebaseStorage", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        View layout;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.WriteExternalStorage) != (int)Permission.Granted)
+            {
+                // Camera permission has not been granted
+                RequestCameraPermission();
+            }
+
+            void RequestCameraPermission()
+            {
+                //Log.Info(TAG, "CAMERA permission has NOT been granted. Requesting permission.");
+
+                if (ActivityCompat.ShouldShowRequestPermissionRationale(this, Manifest.Permission.WriteExternalStorage))
+                {
+                    Snackbar.Make(layout, "Camera permission is needed to show the camera preview.", Snackbar.LengthIndefinite).SetAction("OK", new Action<View>(delegate (View obj)
+                    {
+                        ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.WriteExternalStorage }, 0);
+                    })).Show();
+                }
+                else
+                {
+                    ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.WriteExternalStorage }, 0);
+                }
+            }
+        }
+    }
+}
---
+++ XamarinFirebaseStorage/XamarinFirebaseStorage/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Firebase.Storage;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFirebaseStorage
+{
+    public partial class MainPage : ContentPage
+    {
+        MediaFile file;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Pick_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            try
+            {
+                file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                });
+                if (file == null)
+                    return;
+                imgChoosed.Source = ImageSource.FromStream(() =>
+                {
+                    var imageStram = file.GetStream();
+                    return imageStram;
+                });
+                await StoreImages(file.GetStream());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private async void Store_Clicked(object sender, EventArgs e)
+        {
+            await StoreImages(file.GetStream());
+        }
+
+        public async Task<string> StoreImages(Stream imageStream)
+        {
+            var stroageImage = await new FirebaseStorage("xamarinfirebase-e44c0.appspot.com")
+                .Child("XamarinMonkeys")
+                .Child("image.jpg")
+                .PutAsync(imageStream);
+            string imgurl = stroageImage;
+            return imgurl;
+        }
+    }
+}
